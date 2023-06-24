5791688792f4ba1440d8814f1de2915b8b30d908
hailanglala <hailanglala@gmail.com>
2023-06-24 16:18:49 +0800

    2023-06-24 16:18

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
-<script setup></script>
+<script setup>
+
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const list = ref([])
+
+const getList = async () => {
+  const res = await axios.get('/list')
+  // console.log(res)
+  list.value = res.data
+}
+
+const del = async (id) => {
+  await axios.delete('/del?id=' + id)
+  getList()
+}
+
+onMounted(() => {
+  getList()
+})
+
+</script>
 
 <template>
   <div class="app">
     <el-table :data="list">
-      <el-table-column label="ID"></el-table-column>
-      <el-table-column label="姓名" width="150"></el-table-column>
-      <el-table-column label="籍贯"></el-table-column>
+      <el-table-column label="ID" prop="id"></el-table-column>
+      <el-table-column label="姓名" width="150" prop="name"></el-table-column>
+      <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="100">
-        <template>
-          <el-button type="primary" link >删除</el-button>
+        <template v-slot="{ row }">
+          <el-button type="primary" link @click="del(row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
